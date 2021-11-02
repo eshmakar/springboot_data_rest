9eeeffb36d7d3016f95a897474c57952ef9bd58b
eshmakar <59125020+eshmakar@users.noreply.github.com>
2021-11-02 14:31:23 +0300

    этот проект работает вообще без контроллера, без имплементации интерфейсов и т.д.

--- src/main/java/ru/eshmakar/springboot_data_rest/dao/EmployeeRepository.java
+++ src/main/java/ru/eshmakar/springboot_data_rest/dao/EmployeeRepository.java
@@ -3,5 +3,6 @@ package ru.eshmakar.springboot_data_rest.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.eshmakar.springboot_data_rest.entity.Employee;
 
+//этот проект работает вообще без контроллера, без имплементации интерфейсов и т.д.
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
